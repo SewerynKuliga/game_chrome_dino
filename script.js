d8b0015e27189a2d7e9cbb86f02ae3c76751dd74
Seweryn Kuliga <skuligas@gmail.com>
2021-10-22 13:51:50 +0200

    Update

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 var character = document.querySelector(".character");
 var block = document.querySelector(".block");
-const counter = 0;
+let counter = 0;
 function jump() {
     if (character.classList == "animate") { return }
     character.classList.add("animate");
     setTimeout(() => {
         character.classList.remove("animate")
-    }, 500);
+    }, 300);
 }
 
 const checkDead = setInterval(() => {
     let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if (blockLeft < 30 && blockLeft > -20 && characterTop >= 220) {
+    if (blockLeft < 32 && blockLeft > -20 && characterTop >= 220) {
         block.style.animation = "none";
         alert("Game Over. score: " + Math.floor(counter / 100));
         counter = 0;
         block.style.animation = "block 1s infinite linear";
     } else {
         counter++;
-        document.getElementById(".scoreSpan").innerHTML = Math.floor(counter / 100);
+        document.querySelector(".scoreSpan").innerHTML = Math.floor(counter / 100);
     }
 }, 10);
